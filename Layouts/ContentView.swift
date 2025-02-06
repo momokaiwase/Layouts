7bbc7c47476b8c92d7d87b358b8786dc3aa76e8f
iwasemo <iwasemo@bc.edu>
2025-02-05 20:30:53 -0500

    Ch. 1 SwiftUI Fundamentals Getting Layout Right - Part 3 Divider, Geometry Reader, & safeArea (2025)

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -10,46 +10,60 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = "When the Genius Bar Needs Help, They Call You!"
     var body: some View {
-        VStack {
-            Text("You Have Skills!")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.goldBC)
-                .padding()
-                .background(.maroonBC)
-                //.padding(1)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-            Spacer()
-            Text(messageString)
-                .fontWeight(.heavy)
-                .font(.largeTitle)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-//                .border(.orange, width: 2)
-                .padding()
-  
-            Spacer()
-            
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }
+        //GeometryReader { geometry in
+            VStack {
+                Text("Awesome!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.indigo)
                 Spacer()
-                Button("Great") {
-                    messageString = "You Are Great!"
+                Text(messageString)
+                    .fontWeight(.heavy)
+                    .font(.largeTitle)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 2)
+                    .padding()
+      
+                Spacer()
+                
+//                Divider()
+//                    .background(.black)
+//                    .padding()
+//                    .frame(width: 150.0)
+//                
+//                Rectangle() //rectangle to mimick divider
+//                    .fill(.indigo)
+//                    .frame(width: geometry.size.width * 2/3, height: 1)
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }
+                    Spacer()
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
                 }
-            }
-            .buttonStyle(.borderedProminent)
-//            .border(.purple, width: 5)
-            .padding()
-            .tint(.vermillion)
+                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
+                .tint(.vermillion)
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.indigo)
+            //}
         }
-        .background(
-            Gradient(colors: [.orange, .yellow])
-        )
+        
+        
+        
     }
 }
 
